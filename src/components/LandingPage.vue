2c5037a577ae7bd6de66bc0cf29180a37c63d834
Shiqing <shiqing-thu18@yandex.com>
2020-08-22 16:54:44 +0800

    :heavy_plus_sign: :heavy_minus_sign: Remove antd, add axios and router

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,13 +1,32 @@
 <template>
   <div>Landing page</div>
+  <div>{{ responseText }}</div>
+  <button @click="qwq">Button</button>
+  <button @click="quq">Button</button>
 </template>
 
 <script>
+import { ref } from 'vue';
+import ax from '../axios-instance';
+
 export default {
   name: 'LandingPage',
-  props: {
+  setup() {
+    const responseText = ref('no response');
+    const qwq = () => {
+      ax.get('/hello')
+        .then((resp) => console.log(responseText.value = resp.data));
+    };
+    const quq = () => {
+    };
+
+    return {
+      responseText,
+      qwq,
+      quq,
+    };
   }
-}
+};
 </script>
 
 <style scoped>
